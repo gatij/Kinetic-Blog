4b571fcedba3c06f271263865e4441869cf667ed
gatij <gatijjain@gamil.com>
2018-01-31 23:08:28 +0530

    second commit

--- core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from core.models import Profile,Events,Team
-admin.site.register(Profile)
-admin.site.register(Events)
-admin.site.register(Team)
